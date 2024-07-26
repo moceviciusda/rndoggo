e26132fe9a24c1b3f542da3c520dbe7e84fdff47
moceviciusda <moceviciusdarius@gmail.com>
2024-07-26 14:58:41 +0300

    inject puppers into the DOM

--- src/contentScript/RandomDog.tsx
+++ src/contentScript/RandomDog.tsx
@@ -1,7 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function RandomDog() {
-  const [count, setCount] = useState(0);
+  const [dogSrc, setDogSrc] = useState<string | null>(null);
+  const [refetch, setRefetch] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [containerHovered, setContainerHovered] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetch('https://random.dog/woof')
+      .then((response) => response.text())
+      .then((data) => {
+        setDogSrc(data);
+        setLoading(false);
+      });
+  }, [refetch]);
 
   return (
     <div
@@ -14,28 +28,45 @@ function RandomDog() {
         borderRadius: '0.5rem',
         backgroundColor: 'rgb(43 44 47)',
         borderBottom: '1px solid rgb(32 33 36)',
+        overflow: 'hidden',
+        position: 'relative',
+        maxHeight: 360,
       }}
+      onMouseEnter={() => setContainerHovered(true)}
+      onMouseLeave={() => setContainerHovered(false)}
     >
-      <h1>Vite + React</h1>
-      <div>
-        <button
-          onClick={() => setCount((count) => count + 1)}
-          style={{
-            padding: '0.5rem 1rem',
-            fontSize: '1rem',
-            borderRadius: '0.5rem',
-            backgroundColor: 'rgb(32 33 36)',
-            color: 'white',
-            border: 'none',
-            cursor: 'pointer',
-          }}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+      {dogSrc?.includes('.mp4') ? (
+        <video autoPlay loop muted style={{ objectFit: 'cover' }}>
+          <source src={`https://random.dog/${dogSrc}`} type='video/mp4' />
+        </video>
+      ) : (
+        <img
+          src={`https://random.dog/${dogSrc}`}
+          alt='Random dog'
+          style={{ objectFit: 'cover' }}
+        />
+      )}
+      <button
+        onClick={() => {
+          setRefetch((prev) => !prev);
+        }}
+        disabled={loading}
+        style={{
+          padding: '0.5rem 1rem',
+          marginTop: '1rem',
+          backgroundColor: 'rgb(32 33 36)',
+          color: 'white',
+          border: 'none',
+          borderRadius: '0.5rem',
+          cursor: 'pointer',
+          opacity: containerHovered ? 1 : 0,
+          transition: 'opacity 0.2s',
+          position: 'absolute',
+          bottom: '1rem',
+        }}
+      >
+        {loading ? 'Loading...' : 'New dog 🐶'}
+      </button>
     </div>
   );
 }
